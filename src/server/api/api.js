a339e11007ec478f8bad65a72515ece488de7bd5
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2018-05-08 20:40:33 +0200

    Refactoring: added dsit for ui, moved statics to fs in server

---
+++ src/server/api/api.js
@@ -0,0 +1,8 @@
+const requestHandler = (url, response) => {
+    //TODO: handle API here
+    return response.end();
+};
+
+module.exports = {
+    requestHandler
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,22 @@
 const WebSocketServer = require('websocket').server;
 const http = require('http');
-const statics = require('./statics');
+const statics = require('./fs/statics');
+const api = require('./api/api');
 const projectSourceWatcher = require('./project-source-watcher');
 
 const PORT = 2018;
 const PROJECT_DIR = 'example-project'; //get as param to server script
 
-const httpServer = http.createServer(statics.requestHandler);
+const httpServer = http.createServer((request, response) => {
+    const url = request.url.substr(1);
+
+    if (url.startsWith('api')) {
+        return api.requestHandler(url, response);
+    }
+
+    statics.requestHandler(url, response);
+});
+
 httpServer.listen(PORT, () => {
     console.log(new Date() + `Server is listening localhost: ${PORT}.`);
 });

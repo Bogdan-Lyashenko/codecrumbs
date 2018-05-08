3b37db6e4b98bfa85fe6d526ef620e6711095d66
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2018-05-08 21:40:26 +0200

    Add switchers

--- src/public/js/components/controls/ViewsSwitchList.js
+++ src/public/js/components/controls/ViewsSwitchList.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Switch, Row, Col } from 'antd';
+import { Switch, Form } from 'antd';
+const FormItem = Form.Item;
+
+const Switches = [
+    { name: 'Source Structure', key: 'SourceStructure' },
+    { name: 'Module Dependencies', key: 'ModuleDependencies' },
+    { name: 'Code Crumbs', key: 'CodeCrumbs' }
+];
 
 class ViewsSwitchList extends React.Component {
-    onChange = checked => {
-        console.log(`switch to ${checked}`);
-    };
-
     render() {
         return (
-            <Row>
-                <Col span={6}>
-                    <h3>module dependencies</h3>
-                </Col>
-                <Col span={6}>
-                    <Switch defaultChecked onChange={this.onChange} />
-                </Col>
-            </Row>
+            <Form layout="inline">
+                {Switches.map(item => (
+                    <FormItem key={item.key}>
+                        <FormItem>
+                            <h4>{item.name}</h4>
+                        </FormItem>
+                        <FormItem>
+                            <Switch
+                                defaultChecked
+                                onChange={checked =>
+                                    this.props.onChange(item.key, checked)
+                                }
+                            />
+                        </FormItem>
+                    </FormItem>
+                ))}
+            </Form>
         );
     }
 }

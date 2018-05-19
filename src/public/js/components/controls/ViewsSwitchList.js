a97a1323200a23fd74b2e9b2711ca452c1fff72e
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2018-05-19 11:50:31 +0200

    Add Dependencies tree switch

--- src/public/js/components/controls/ViewsSwitchList.js
+++ src/public/js/components/controls/ViewsSwitchList.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { Switch, Form, Icon } from 'antd';
 const FormItem = Form.Item;
+import './ViewSwitchList.css';
+
+export const SWITCH_KEYS = {
+    SOURCE: 'SourceStructure',
+    DEPENDENCIES: 'ModuleDependencies',
+    CODE_CRUMBS: 'CodeCrumbs'
+};
 
 const Switches = [
-    { name: 'Source Structure', key: 'SourceStructure' },
-    { name: 'Module Dependencies', key: 'ModuleDependencies' },
-    { name: 'Code Crumbs', key: 'CodeCrumbs' }
+    { name: 'Source', key: SWITCH_KEYS.SOURCE },
+    { name: 'Dependencies', key: SWITCH_KEYS.DEPENDENCIES },
+    { name: 'CodeCrumbs', key: SWITCH_KEYS.CODE_CRUMBS }
 ];
 
-class ViewsSwitchList extends React.Component {
-    render() {
-        return (
+const ViewsSwitchList = ({ onChange, defaultChecked }) => {
+    return (
+        <div className="ViewSwitchList-container">
             <Form layout="inline">
                 {Switches.map(item => (
                     <FormItem key={item.key}>
                         <span>{item.name + ' '}</span>
                         <Switch
-                            defaultChecked
-                            onChange={checked =>
-                                this.props.onChange(item.key, checked)
-                            }
+                            size="small"
+                            defaultChecked={item.key === defaultChecked}
+                            onChange={checked => onChange(item.key, checked)}
                         />
                     </FormItem>
                 ))}
             </Form>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default ViewsSwitchList;

beb34935a2dec83d1276748a7f9eedb98e23ba9d
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2018-05-29 22:07:09 +0200

    Add hjs

--- src/public/js/components/side-bar/component/Code/Code.js
+++ src/public/js/components/side-bar/component/Code/Code.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { atomOneLight } from 'react-syntax-highlighter/styles/hljs';
+import { hybrid } from 'react-syntax-highlighter/styles/hljs';
 
 //TODO: scrool to/highlight crumbed lines
 //https://github.com/conorhastings/react-syntax-highlighter/blob/master/README.md
 export default ({ code, crumbedLines = [] }) => (
     <SyntaxHighlighter
         language="javascript"
-        style={atomOneLight}
-        customStyle={{ fontSize: '13px' }}
+        style={hybrid}
+        customStyle={{ fontSize: '14px' }}
+        showLineNumbers={true}
     >
         {code}
     </SyntaxHighlighter>

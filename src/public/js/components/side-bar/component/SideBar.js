545187e9fa4e40574e4d58c372b3d49867c716ae
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2018-05-29 22:04:43 +0200

    Add hjs

--- src/public/js/components/side-bar/component/SideBar.js
+++ src/public/js/components/side-bar/component/SideBar.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import { Tabs } from 'antd';
+const TabPane = Tabs.TabPane;
 import './SideBar.css';
-import { Card } from 'antd';
+import Code from './Code/Code';
 
 //TODO: Add slide from right animation
 export default ({ file, codeCrumb, onClose }) => {
+    const crumbedLines = !codeCrumb
+        ? []
+        : codeCrumb.crumbedLineNode.loc.start.line;
+
     return (
         <div className="SideBar-container">
-            <Card
-                title={file.path}
-                extra={
-                    <a href="#" onClick={onClose}>
-                        [X]
-                    </a>
-                }
-            >
-                <p>file: {file.name}</p>
-                {codeCrumb && <p>crumb: {codeCrumb.name}</p>}
-            </Card>
+            <div className="SideBar-header">
+                <div>{file.path}</div>
+                <a href="#" onClick={onClose}>
+                    X
+                </a>
+            </div>
+
+            <div className="SideBar-body">
+                {file.fileCode && (
+                    <Tabs defaultActiveKey="1" onChange={() => {}}>
+                        <TabPane tab="Code" key="1">
+                            <Code
+                                code={file.fileCode}
+                                crumbedLines={crumbedLines}
+                            />
+                        </TabPane>
+                        <TabPane tab="FlowChart" key="2">
+                            Content of Tab Pane 2
+                        </TabPane>
+                        <TabPane tab="Crumbs" key="3">
+                            Content of Tab Pane 3
+                        </TabPane>
+                    </Tabs>
+                )}
+            </div>
         </div>
     );
 };

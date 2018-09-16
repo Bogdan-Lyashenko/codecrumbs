091bffcb524a651e5749cfcfe6a20259288a2de2
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2018-09-16 11:26:22 +0200

    Perf fixes for file nodes

--- src/public/js/components/treeDiagram/component/Tree/DependenciesTree.js
+++ src/public/js/components/treeDiagram/component/Tree/DependenciesTree.js
@@ -7,23 +7,6 @@ import {
 import { FileName } from 'components/treeDiagram/component/Node/File';
 import { DepEdgeGroups } from 'components/treeDiagram/store/constants';
 
-export const getFilteredDependenciesList = ({
-  dependenciesList,
-  dependenciesEntryPoint,
-  dependenciesShowDirectOnly
-}) => {
-  if (!dependenciesEntryPoint) {
-    return [];
-  }
-
-  if (dependenciesShowDirectOnly) {
-    const dependency = dependenciesList.find(d => d.moduleName === dependenciesEntryPoint.path);
-    return dependency ? [dependency] : [];
-  }
-
-  return collectDependencies(dependenciesEntryPoint.path, dependenciesList);
-};
-
 export const getGroupsAroundNode = (moduleNode, importedNodes) => {
   const groups = {
     [DepEdgeGroups.TOP_LEFT]: [],
@@ -70,29 +53,6 @@ export const getGroupsAroundNode = (moduleNode, importedNodes) => {
   return groups;
 };
 
-export const collectDependencies = (entryModuleName, dependenciesList) => {
-  let queue = [].concat(entryModuleName),
-    store = [];
-
-  while (queue.length) {
-    let moduleName = queue.shift(),
-      entryModule = dependenciesList.find(d => d.moduleName === moduleName);
-
-    if (entryModule) {
-      store.push(entryModule);
-
-      const nodeBody = entryModule.importedModuleNames;
-      if (nodeBody) {
-        queue = [...queue, ...nodeBody];
-      }
-    } else {
-      console.error('looks like ' + entryModuleName + 'is not imported anywhere');
-    }
-  }
-
-  return store;
-};
-
 const checkIsEdgeSelected = (selectedEdge, target, source) => {
   if (selectedEdge.target && selectedEdge.sources.length > 1) {
     return selectedEdge.target === target;
@@ -108,14 +68,13 @@ const checkIsEdgeSelected = (selectedEdge, target, source) => {
 class DependenciesTree extends React.Component {
   render() {
     const {
+      filteredDependenciesList,
       fileNodesMap,
       shiftToCenterPoint,
       sourceDiagramOn,
       onDependencyEdgeClick,
       selectedDependencyEdgeNodes
     } = this.props;
-
-    const filteredDependenciesList = getFilteredDependenciesList(this.props);
 
     return (
       <React.Fragment>

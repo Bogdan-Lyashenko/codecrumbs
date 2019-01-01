eb084c8a8cef46b50b9dd2ecef81ab866e698f56
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2019-01-01 18:53:32 +0100

    Fix public path for prod

--- src/public/webpack.prod.js
+++ src/public/webpack.prod.js
@@ -3,10 +3,8 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  entry: [path.resolve(__dirname, 'js/public-path.js'), path.resolve(__dirname, 'js/index.js')],
   output: {
     path: path.resolve(__dirname, 'dist/standalone/bundle/'),
-    publicPath: '',
   },
   mode: 'production'
 });

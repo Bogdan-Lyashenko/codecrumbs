66bcef28c60d9d0fba0f5f153b388ebc7e94c633
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2019-01-01 18:48:15 +0100

    Fix public path for prod

--- src/public/js/public-path.js
+++ src/public/js/public-path.js
@@ -1,2 +1,4 @@
 const isLocalHost = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
-__webpack_public_path__ = isLocalHost ? '/bundle/' : '/src/public/dist/standalone/bundle/';
+__webpack_public_path__ = isLocalHost
+  ? '/bundle/'
+  : '/codecrumbs/src/public/dist/standalone/bundle/';

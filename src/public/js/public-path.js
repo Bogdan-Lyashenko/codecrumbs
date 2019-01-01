bfa0c4d9eb070e227de3f4dabb2025eb48488101
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2019-01-01 18:38:48 +0100

    Fix public path for prod

---
+++ src/public/js/public-path.js
@@ -0,0 +1,2 @@
+const isLocalHost = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
+__webpack_public_path__ = isLocalHost ? '/bundle/' : '/src/public/dist/standalone/bundle/';
--- src/public/webpack.prod.js
+++ src/public/webpack.prod.js
@@ -3,9 +3,10 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  entry: [path.resolve(__dirname, 'js/public-path.js'), path.resolve(__dirname, 'js/index.js')],
   output: {
     path: path.resolve(__dirname, 'dist/standalone/bundle/'),
-    publicPath: undefined,
+    publicPath: '',
   },
   mode: 'production'
 });

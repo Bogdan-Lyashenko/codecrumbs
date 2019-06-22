cad0113d598f5a5654edb9d6a1462e4dfdf24864
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2019-06-22 20:15:07 +0200

    feat: add integration for io

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -7,6 +7,24 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import getStore from 'core/store';
 
+export const getIntegration = () => {
+  const { store, persistor } = getStore();
+
+  const Codecrumbs = options => (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App {...options} />
+      </PersistGate>
+    </Provider>
+  );
+
+  return {
+    Codecrumbs,
+    store,
+    persistor
+  };
+};
+
 export default (options, mountNodeId) => {
   const { store, persistor } = getStore();
 

0cf67aa8bbd70adce973a1eec23b0c1fc9c65793
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2019-06-30 21:09:45 +0200

    fix: babel load

--- src/public/babel.config.js
+++ src/public/babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function(app) {
   app.cache(true);
 
-  const presets = ['@babel/preset-env', '@babel/preset-react'];
+  const presets = [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage'
+      }
+    ]
+  ];
 
   const plugins = [
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],

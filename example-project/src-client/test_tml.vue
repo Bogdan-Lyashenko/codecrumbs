770ae6d5cf8c067add3ee691b190be4a088d982f
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2019-07-08 20:53:13 +0200

    fix: add test vue file

---
+++ example-project/src-client/test_tml.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+  module.exports = {
+    data: function () {
+      return {
+        greeting: 'Hello'
+      }
+    }
+  }
+</script>
+
+//cc:test vue
+<style scoped>
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
+</style>

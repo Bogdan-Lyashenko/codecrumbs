3e5da523ce5377a13f158cf1a58d4c77b92aacaa
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-07-23 14:00:05 +0200

    chore: import only getCrumbs

--- src/server/code-parse/language/golang/codecrumbs.js
+++ src/server/code-parse/language/golang/codecrumbs.js
@@ -1,7 +1,4 @@
-const { setupGetCrumbs, setupGetCommentsFromCode } = require('../default/codecrumbs');
-
-const GOLANG_COMMENT_REGEX = /^([^\/\/]*)\/\/(.*)$/
-const getCrumbs = setupGetCrumbs(setupGetCommentsFromCode(GOLANG_COMMENT_REGEX));
+const { getCrumbs } = require('../default/codecrumbs');
 
 // replace with own implementation if needed
 module.exports = {
--- src/server/code-parse/language/typescript/codecrumbs.js
+++ src/server/code-parse/language/typescript/codecrumbs.js
@@ -1,7 +1,4 @@
-const { setupGetCrumbs, setupGetCommentsFromCode } = require('../default/codecrumbs');
-
-const TYPE_SCRIPT_COMMENT_REGEX = /^([^\/\/]*)\/\/(.*)$/;
-const getCrumbs = setupGetCrumbs(setupGetCommentsFromCode(TYPE_SCRIPT_COMMENT_REGEX));
+const { getCrumbs } = require('../default/codecrumbs');
 
 // replace with own implementation if needed
 module.exports = {

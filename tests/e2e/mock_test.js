467ab7d9d76cb37224c3a309bb1bbbc4ea95775d
Christy Jacob <christyjacob4@gmail.com>
2019-08-21 02:17:50 +0530

    feat: added page-model and some test-scripts
    Started building the page model with some basic functionality and added a few tests to check the working of some buttons.

--- tests/e2e/mock_test.js
+++ tests/e2e/mock_test.js
@@ -1,16 +1,94 @@
-import { Selector } from 'testcafe';
+import Page from '../page-objects/page-model';
+import {
+  Selector
+} from 'testcafe';
 
-fixture `Getting Started`
-    .page `http://${process.env.PLAYGROUND_URL}`;
 
+fixture `My fixture`
+  .page `http://${process.env.PLAYGROUND_URL}`;
 
-test('Sidebar Button Test', async t => {
+const page = new Page();
+
+// Mock Test
+test('Mock Test', async t => {
   await t
-      .click('#mount-node > div > header > div.ViewSwitchList > div:nth-child(2) > div:nth-child(1) > div > div > button');
+    .click(page.getSwitch('Source'))
+    .click(page.getSwitch('Dependencies'))
+    .click(page.getSwitch('CodeCrumbs'))
+    .click(page.getSwitch('SideBar'))
 });
 
-test('Dependency Button Test', async t => {
+
+test('Clicking Source Toggle', async t => {
+  const button = await page.getSwitch('Source');
   await t
-      .click('#mount-node > div > header > div.ViewSwitchList > div:nth-child(1) > div:nth-child(2) > div > div > button');
+    .click(button)
+  // Check if button is set to false
+    .expect(button.withAttribute(/^aria-checked$/, /^false$/).exists).ok()
+  // Check if dropdown is disabled
+  await page.isDropDownDisabled('Source')
+  // Check if the diagram is removed
+  await page.isTreeDiagramEmpty()
+  // expect menu to disappear
+
+  await t.click(button)
+  // Check if the button is set to true
+    .expect(button.withAttribute(/^aria-checked$/, /^true$/).exists).ok()
+  // Click the dropdown
+    .click(button.parent(0))
+  // Check if the dropdown has appeared
+  await page.isDropDownEnabled('Source')
+  // Check if the diagram has appeared
+  await page.isTreeDiagramPopulated()
+  // expect menu to appear
 });
 
+test('Clicking Dependencies Toggle', async t => {
+  const button = await page.getSwitch('Dependencies');
+
+  await t.click(button)
+  // Check if button is set to true
+    .expect(button.withAttribute(/^aria-checked$/, /^true$/).exists).ok()
+  // Click the dropdown
+    .click(button.parent(0))
+  // Check if Dropdown menu appeared
+  await page.isDropDownEnabled('Dependencies')
+
+  await t
+    .click(button)
+  // Check if button is set to false
+    .expect(button.withAttribute(/^aria-checked$/, /^false$/).exists).ok()
+  // Check if Dropdown menu disappeared
+  await page.isDropDownDisabled('Dependencies')
+});
+
+test('Clicking CodeCrumbs Toggle', async t => {
+  const button = await page.getSwitch('CodeCrumbs');
+
+  await t.click(button)
+  // Check if button is set to true
+    .expect(button.withAttribute(/^aria-checked$/, /^true$/).exists).ok()
+  // Click the dropdown
+    .click(button.parent(0))
+  // Check if Dropdown menu appeared
+  await page.isDropDownEnabled('CodeCrumbs')
+
+  await t
+    .click(button)
+  // Check if button is set to false
+    .expect(button.withAttribute(/^aria-checked$/, /^false$/).exists).ok()
+  // Check if Dropdown menu disappeared
+  await page.isDropDownDisabled('CodeCrumbs')
+
+});
+
+test('Clicking SideBar Toggle', async t => {
+  const button = await page.getSwitch('SideBar');
+
+  await t.click(button)
+  // Check if button is set to true
+    .expect(button.withAttribute(/^aria-checked$/, /^true$/).exists).ok()
+    .click(button)
+  // Check if button is set to false
+    .expect(button.withAttribute(/^aria-checked$/, /^false$/).exists).ok()
+});

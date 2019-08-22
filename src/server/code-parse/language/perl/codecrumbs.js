6724b83de23f4a17d661cfa27a3ceb6d693fe588
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-08-22 13:30:43 +0200

    feat: support for multiline comment for languages that use default regex

--- src/server/code-parse/language/perl/codecrumbs.js
+++ src/server/code-parse/language/perl/codecrumbs.js
@@ -1,6 +1,6 @@
 const { setupGetCrumbs, setupGetCommentsFromCode } = require('../default/codecrumbs');
 
-const PERL_COMMENT_REGEX = /^([^#]*)#(.*)$/g;
+const PERL_COMMENT_REGEX = /(^=begin(.*?|\n)*?=cut$|#.*)/gm
 const getCrumbs = setupGetCrumbs(setupGetCommentsFromCode(PERL_COMMENT_REGEX));
 
 // replace with own implementation if needed

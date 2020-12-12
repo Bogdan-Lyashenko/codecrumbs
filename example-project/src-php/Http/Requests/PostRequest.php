cf8ba61943457cdd8ce6002c0c39e726fff6ffee
syarig <syarig110@yahoo.co.jp>
2020-12-12 23:29:49 +0900

    chore: remove example project code in src-php

--- example-project/src-php/Http/Requests/PostRequest.php
+++ example-project/src-php/Http/Requests/PostRequest.php
@@ -8,32 +8,8 @@ use App\Rules\Category;
 
 class PostRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        if ($this->path !== 'post') {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-//            'post.category_id' => new Category,
-            'post.thumb' => 'between:1,255',
-            'post.title' => 'between:0,255',
-            'post.content' => 'between:0,8191'
-        ];
+      // authorize
     }
 }
--- example-project/src-php/Models/Category.php
+++ example-project/src-php/Models/Category.php
@@ -8,6 +8,6 @@ class Category extends Model
 {
     public function posts()
     {
-        return $this->hasMany('App\Post');
+      // posts
     }
 }
